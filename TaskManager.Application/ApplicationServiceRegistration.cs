1b61abc3aa89bdc251ea941ad1545b51742408cd
Guchikata <38571128+errkedukke@users.noreply.github.com>
2025-07-08 19:15:10 +0400

    Taskmanager: Updated nuget packages.

--- TaskManager.Application/ApplicationServiceRegistration.cs
+++ TaskManager.Application/ApplicationServiceRegistration.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using TaskManager.Application.Contracts.BackgroundServices;
+using TaskManager.Application.Mapping_Profiles;
 using TaskManager.Application.Service;
 
 namespace TaskManager.Application;
@@ -12,8 +13,13 @@ public static class ApplicationServiceRegistration
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
         var executionAssembly = Assembly.GetExecutingAssembly();
+
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.AddMaps(typeof(TaskManagerProfile).Assembly);
+        });
+
         services.AddMediatR(x => x.RegisterServicesFromAssembly(executionAssembly));
-        services.AddAutoMapper(executionAssembly);
         services.AddScoped<ITaskItemAssignmentService, TaskItemAssignmentService>();
         services.AddValidatorsFromAssembly(executionAssembly);
 
